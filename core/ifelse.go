02f3e6cf1025b054b0bfeed33cc2de759ce62596
isaac <44104081+isaacwilkes@users.noreply.github.com>
2021-01-20 22:45:11 +0000

    Update ifelse.go

--- core/ifelse.go
+++ core/ifelse.go
@@ -5,14 +5,14 @@ import (
 )
 
 func main() {
-	a := [10]int{25, 33, 17, 28, 37, 26, 28, 36, 68, 50}
+	a := [15]int{25, 33, 17, 28, 37, 26, 28, 36, 68, 50, 34, 46, 474, 84, 29}
 	fmt.Println(a)
 
 	for i := 0; i < len(a); i++ {
 		if a[i]%2 == 0 {
-			fmt.Println(a[i], "is even")
+			fmt.Println(a[i], "is even number")
 		} else {
-			fmt.Println(a[i], "is odd")
+			fmt.Println(a[i], "is odd number")
 		}
 	}
 
@@ -23,9 +23,9 @@ func main() {
 	fmt.Println(result)
 
 	if result%2 == 0 {
-		fmt.Println("the sum of numbers in array is even:", result)
+		fmt.Println("the sum of numebers in array is even:", result)
 	} else {
-		fmt.Println("the sum of numbers in array is odd:", result)
+		fmt.Println("the sum of numebers in array is odd:", result)
 	}
 
 }
