fd5dad71c62d12b697085bf45e69e68fb5cbee23
isaac <44104081+isaacwilkes@users.noreply.github.com>
2021-01-23 00:27:59 +0000

    Create error.go

---
+++ core/error.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	middle, total, err := multiply(10, 5)
+
+	if err != nil {
+		fmt.Println("Error.", err)
+	} else {
+		fmt.Println(total)
+		fmt.Println("median of original numbers", middle)
+	}
+
+}
+
+func multiply(start, end int) (float64, int, error) {
+
+	if start > end {
+		return 0, 0, errors.New("Invalid")
+	}
+
+	total := 1
+	for i := start; i <= end; i++ {
+		total *= i
+	}
+
+	middle := (float64(start) + float64(end)) / 2
+	return middle, total, nil
+}
